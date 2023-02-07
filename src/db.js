bbdbc51fbfb04a131571c8a25797d2f30078dabd
bmehder <bmehder@gmail.com>
2023-02-07 22:22:40 +0000

    made components and db

---
+++ src/db.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    name: 'Vapormax Plus',
+    color: 'black',
+  },
+  {
+    name: 'Vapormax',
+    color: 'asphalt',
+  },
+  {
+    name: 'Vapormax',
+    color: 'Grape',
+  },
+  {
+    name: 'AirMax',
+    color: 'Leopard',
+  },
+  {
+    name: 'AirMax',
+    color: 'wheat',
+  },
+  {
+    name: 'Flyknit',
+    color: 'zebra',
+  },
+  {
+    name: 'Air Max 270',
+    color: 'React camo',
+  },
+  {
+    name: 'Airmax 90',
+    color: 'Galaxy',
+  },
+  {
+    name: 'Airmax 270',
+    color: 'Black Gold',
+  },
+]
